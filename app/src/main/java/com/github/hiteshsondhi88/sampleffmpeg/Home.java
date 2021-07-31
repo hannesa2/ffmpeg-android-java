6c0f5b8f3e0b6fc7ef2265e437335064a6ac80a8
Hannes Achleitner <hannes.software@gmx.at>
2021-07-31 12:57:28 +0200

    Android X

--- app/src/main/java/com/github/hiteshsondhi88/sampleffmpeg/Home.java
+++ app/src/main/java/com/github/hiteshsondhi88/sampleffmpeg/Home.java
@@ -7,8 +7,8 @@ import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.support.v4.app.ActivityCompat;
-import android.text.TextUtils;
+import androidx.core.app.ActivityCompat;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
