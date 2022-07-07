595a163f78546e62836715d04d3ca1472dbef852
Hannes Achleitner <hannes.software@gmx.at>
2022-07-07 06:29:01 +0200

    Fix NDK build

--- FFmpegAndroid/src/main/jni/Android.mk
+++ FFmpegAndroid/src/main/jni/Android.mk
@@ -6,10 +6,10 @@ LOCAL_MODULE := ARM_ARCH
 
 LOCAL_SRC_FILES := armArch.c
 
-LOCAL_CFLAGS := -DHAVE_NEON=1
-LOCAL_STATIC_LIBRARIES := cpufeatures
+#LOCAL_CFLAGS := -DHAVE_NEON=1
+#LOCAL_STATIC_LIBRARIES := cpufeatures
 
-LOCAL_LDLIBS := -llog
+#LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)
-$(call import-module,cpufeatures)
+#$(call import-module,cpufeatures)
